36fb07a83fa5b058998d50c56099df37d7329994
Mickal Austin <maustin034@west-mec.org>
2019-02-28 15:26:49 -0700

    Added code but not working
    something wrong with code but needs to be fixed tomorrow

---
+++ tc_cart.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: mickal austin
+   Date:  2.27.19 
+   
+   Filename: tc_cart.js
+	
+*/
+//variable for total order
+var orderTotal = [0];
+//variable of html for cart
+var cartHTML = "< table > < tr > < th > Item < /th><th>Description</th > < th > Price < /th><th>Qty</th > < th > Total < /th> </tr >";
+//loop that loops through entries
+for (var i = 0; i < item.length; i++) {
+    cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "' /></td>";
+    cartHTML += "<td>" + itemDescription + "</td> <td>$" + itemPrice + "</td> <td>" + itemQty + "</td>";
+    var itemCost = itemPrice[i] * itemQty[i];
+    cartHTML += "<td>$" + itemCost + "</td></tr>";
+    orderTotal += itemCost;
+}
+
+var cartHTML = "<tr> <td colspan='4'>Subtotal</td> <td>$" + orderTotal + "</td> </tr> </table>";
+document.getElementById("cart").innerHTML = cartHTML;
