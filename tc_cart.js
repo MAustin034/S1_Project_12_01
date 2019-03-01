e8c83b3c86d6e823dfc95608dcdc46f5c0fb8877
Mickal Austin <maustin034@west-mec.org>
2019-03-01 13:58:20 -0700

    Fixed problems now its ready to be graded

--- tc_cart.js
+++ tc_cart.js
@@ -12,17 +12,17 @@
 	
 */
 //variable for total order
-var orderTotal = [0];
+var orderTotal = 0;
 //variable of html for cart
-var cartHTML = "< table > < tr > < th > Item < /th><th>Description</th > < th > Price < /th><th>Qty</th > < th > Total < /th> </tr >";
+var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>";
 //loop that loops through entries
-for (var i = 0; i < item.length; i++) {
-    cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "' /></td>";
-    cartHTML += "<td>" + itemDescription + "</td> <td>$" + itemPrice + "</td> <td>" + itemQty + "</td>";
+for (var i = 0; i < 4; i++) {
+    cartHTML += "<tr> <td><img src='tc_" + item[i] + ".png' alt='item' /></td>";
+    cartHTML += "<td>" + itemDescription[i] + "</td> <td>$" + itemPrice[i] + "</td> <td>" + itemQty[i] + "</td>";
     var itemCost = itemPrice[i] * itemQty[i];
     cartHTML += "<td>$" + itemCost + "</td></tr>";
     orderTotal += itemCost;
 }
 
-var cartHTML = "<tr> <td colspan='4'>Subtotal</td> <td>$" + orderTotal + "</td> </tr> </table>";
+cartHTML += "<tr> <td colspan='4'>Subtotal</td> <td>$" + orderTotal + "</td> </tr> </table>";
 document.getElementById("cart").innerHTML = cartHTML;
